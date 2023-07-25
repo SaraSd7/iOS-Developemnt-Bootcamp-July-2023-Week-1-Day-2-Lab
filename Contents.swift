0c9a7e7229e743a345dc285c3c7b0f66d01757c7
Sara AlDossari <97613373+SaraSd7@users.noreply.github.com>
2023-07-25 17:02:50 +0300

    Add files via upload

---
+++ Contents.swift
@@ -0,0 +1,27 @@
+import UIKit
+
+var score: Int = 2
+
+score += 10
+print(score)
+
+if score > 9 {
+    print("Pass")
+} else {
+    print("Fail")
+}
+
+var arr = ["Sara", "Haya","Layan","Lujain","Amal"]
+
+for i in arr {
+   print(i)
+}
+
+var fruit = ["apple":3, "orange":2, "banana":4,"kiwi":5, "peach":2]
+
+for i in fruit {
+    print(i)
+}
+
+let name = "Sara"
+print("Welcome \(name)")
